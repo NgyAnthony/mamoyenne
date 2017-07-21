7971cdc79cd354ddd5594d543511e7bc53027504
NgyAnthony <anthony.ng@hotmail.fr>
2017-07-21 18:02:39 +0200

    test

--- test/no.js
+++ test/no.js
@@ -2,19 +2,18 @@
  * Created by Anthony on 07/06/2017.
  */
 /* -----CALCUL---- */
+//Valeurs brutes reçues sur la table
+var moyenneNum = [];
+var moyenneDen = [];
+var moyenneCoef = [];
 function calcul() {
     //Valeurs brutes converties pour pouvoir faire le calcul (valeurs net)
     var moyenneList = [];
     var coefList = [];
     var sommeCoef = 0;
 
-    //Valeurs brutes reçues sur la table
-    var moyenneNum = [];
-    var moyenneDen = [];
-    var moyenneCoef = [];
-
     //Push des valeurs brutes en valeurs net
-    for(var a=0; a<moyenneNum.length && a< moyenneDen.length ; a++){
+    for(var a=0; a<moyenneNum.length && a<moyenneDen.length ; a++){
         moyenneList.push(moyenneNum[a]/moyenneDen[a]);
     }
 
@@ -31,22 +30,10 @@ function calcul() {
 
     var avg = total / sommeCoef;
     var result = avg * 20;
+    console.log(result)
 }
 
 
-//Ajout boutton/Clone
-var pressedNumber = 0;
-function counter() {
-    pressedNumber = pressedNumber+1;
-}
-
-var moyenne = {
-    init: function(numerateur,denominateur,coefficient){
-        this.numerateur = numerateur;
-        this.denominateur = denominateur;
-        this.coefficient = coefficient;
-    }
-};
 
 function add(){
         var div = document.getElementById('form');
@@ -54,31 +41,30 @@ function add(){
         num.setAttribute("type", "number");
         num.setAttribute("placeholder", "Numerateur");
         num.setAttribute("name", "numerateur");
-        num.setAttribute("id", pressedNumber);
     div.appendChild(num);
 
     var den = document.createElement("INPUT");
         den.setAttribute("type", "number");
         den.setAttribute("placeholder", "Denominateur");
         den.setAttribute("name", "denominateur");
-        num.setAttribute("id", pressedNumber);
     div.appendChild(den);
 
     var coef = document.createElement("INPUT");
         coef.setAttribute("type", "number");
         coef.setAttribute("placeholder", "Coefficient");
         coef.setAttribute("name", "coefficient");
-        num.setAttribute("id", pressedNumber);
     div.appendChild(coef);
 }
 
-function pushCalcul(){
-    for (var i = 0; i < pressedNumber; i++) {
-        var moy = Object.create(moyenne);
-        moy.init(document.getElementsByName(numerateur), document.getElementsByName(denominateur), document.getElementsByName(coefficient));
-    }
-    moyenneNum.push(moy(document.getElementsByName(numerateur)));
-    moyenneDen.push(moy(document.getElementsByName(denominateur)));
-    moyenneCoef.push(moy(document.getElementsByName(coefficient)));
-    console.log(result)
+function push(){
+    var num = document.getElementsByName("numerateur");
+    var den = document.getElementsByName("denominateur");
+    var coef = document.getElementsByName("coefficient");
+    moyenneNum.push(num);
+    moyenneDen.push(den);
+    moyenneCoef.push(coef);
 }
+
+
+
+/* La fonction pushCalcul() push les variables JS de la fonction add() qui créent les inputs HTML dans les tableaux qui sont dans la fonciton calcul() */
